44631df02e5e7c60419d790e074962de6c2ffd07
Jason Lu <jasonlu2907@gmail.com>
2022-05-12 12:45:14 -0500

    Automate postgres restart & prisma migrations, Nest ConfigModule, NEXT UP: passport.js & JWT

--- prisma/migrations/20220512172057_update_models/migration.sql
+++ prisma/migrations/20220512172057_update_models/migration.sql
@@ -1,7 +1,9 @@
 /*
   Warnings:
+
   - You are about to drop the `Bookmark` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
 */
 -- DropTable
 DROP TABLE "Bookmark";
